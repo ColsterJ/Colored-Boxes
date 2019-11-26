7e19406a38cf383f5de352728132961973633a7b
Colin Jones <colintjones@gmail.com>
2019-11-26 08:51:28 -0500

    Uses state to change background color, instead of setting directly

--- src/App.js
+++ src/App.js
@@ -1,48 +1,23 @@
 import React from 'react';
+import Square from './Square'
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props); 
-    
-    this.state = {
-      backgroundColor: 0xFFC0CB, 
-    };
-
-    // Only changeColor needs to be bound (not randomColor), because that's what is used in the in render()
-    this.changeColor = this.changeColor.bind(this);
-  }
-
-
-  randomColor() {
-    return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
-  }
-
-  changeColor(event) {
-    // this.setState({backgroundColor: this.randomColor()});
-    event.target.style.backgroundColor = this.randomColor();
-  }
-
   render() {     
-
-
     return (
         <div className="Container">
-          {/* <div className="Square" onMouseEnter={this.changeColor} style={{backgroundColor: this.state.backgroundColor}}></div> */}
-          <div className="Square" onMouseOver={this.changeColor}></div>
-          <div className="Square" onMouseOver={this.changeColor}></div>
-          <div className="Square" onMouseOver={this.changeColor}></div>
-          <div className="Square" onMouseOver={this.changeColor}></div>
-          <div className="Square" onMouseOver={this.changeColor}></div>
-          <div className="Square" onMouseOver={this.changeColor}></div>
-          <div className="Square" onMouseOver={this.changeColor}></div>
-          <div className="Square" onMouseOver={this.changeColor}></div>
-          <div className="Square" onMouseOver={this.changeColor}></div>
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
         </div>
     );
   }
 }
-
-
 
 export default App;
---
+++ src/Square.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Square.css'
+
+class Square extends React.Component {
+    constructor(props) {
+        super(props); 
+        
+        this.state = {
+          backgroundColor: 0xFFC0CB, 
+        };
+    
+        // Only changeColor needs to be bound (not randomColor), because that's what is used in the in render()
+        this.changeColor = this.changeColor.bind(this);
+      }
+    
+    
+      randomColor() {
+        return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+      }
+    
+      changeColor(event) {
+        this.setState({backgroundColor: this.randomColor()});
+      }       
+
+      render() {
+        return (
+            <div
+                className="Square"
+                onMouseOver={this.changeColor}
+                style={{backgroundColor: this.state.backgroundColor}}
+            >
+            </div>
+        );
+      }
+}
+export default Square;
